39d5558ba0cbc2a4c4e95e584cd7629892981b04
balakrishna <balakrishna.kunthumuri@thundersoft.com>
2021-08-19 13:51:28 +0530

    commit for LSB

---
+++ f1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+
+    /* Input number from user */
+    printf("Enter any number: ");
+    scanf("%d", &n);
+
+    /* If (n&1) evaluates to 1 */
+    if(n&1)
+    {
+        printf("LSB of %d is set (1).", n);
+    }
+    else
+    {
+        printf("LSB of %d is unset (0).", n);
+    }
+    return 0;
+}
+
